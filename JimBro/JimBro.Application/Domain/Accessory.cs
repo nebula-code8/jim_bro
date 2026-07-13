14ae15d7b4fea380cb2487155009e05a25c9f950
DmitrNKL <work.hiorolink.21@gmail.com>
2026-07-13 17:57:14 +0200

    [feat] Add Accessory domain model and repository interface

---
+++ JimBro/JimBro.Application/Domain/Accessory.cs
@@ -0,0 +1,24 @@
+﻿namespace JimBro.Domain;
+
+public class Accessory
+{
+    public long Id { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public double? Weight { get; private set; }
+    
+    public Accessory(string name, string description, double height, double? weight)
+    {
+        Name = name;
+        Description = description;
+        Weight = weight;
+    }
+    
+    public Accessory(long id, string name, string description, double height, double? weight)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Weight = weight;
+    }
+}
---
+++ JimBro/JimBro.Application/Domain/RepositoryInterfaces/IAccessoryRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace JimBro.Domain.RepositoryInterfaces;
+
+public interface IAccessoryRepository
+{
+    List<Accessory> GetAllAccessories();
+    long Insert(Accessory accessory);
+    int Update(Accessory accessory);
+    int Delete(long id);
+}
